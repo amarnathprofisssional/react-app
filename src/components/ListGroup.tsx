be7a66ba142dd5e0379390cff50d2d8aee7cc1f2
amarnathprofisssional <amarnathprofissional@gmail.com>
2023-09-23 15:48:14 +0530

    initi commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,23 @@
+import { MouseEvent } from "react";
+
+function ListGroup() {
+  let items = ["New York", "Chennai", "Tokyo", "London", "Paris"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
